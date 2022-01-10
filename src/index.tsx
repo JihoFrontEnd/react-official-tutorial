a44b442454fd98c55203438d1e1720480c3b391f
Jihogrammer <jiho@kakao.com>
2022-01-10 13:49:49 +0900

    learn: Declaring a Winner

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -22,19 +23,32 @@ function Square(props: SquareProps) {
 function Board(props: Record<string, string>) {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const [status, setStauts] = useState('');
+
   const handleClick = (i: number) => {
     const newSquares = squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) return;
     newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
     setXIsNext(!xIsNext);
   };
+
   const renderSquare = (i: number) => (
     <Square
       value={squares[i]}
       onClick={() => handleClick(i)}
     />
   );
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  useEffect(() => {
+    const winner = calculateWinner(squares);
+    setStauts(
+      winner
+        ? `Winner: ${winner}`
+        : `Next player: ${xIsNext ? 'X' : 'O'}`
+    );
+  }, [squares, xIsNext]);
+
   return (
     <div>
       <div className="status">{status}</div>
@@ -71,6 +85,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares: any[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
